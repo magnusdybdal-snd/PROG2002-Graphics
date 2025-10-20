ea0fb06330d9226a67e9eaf84894799a0366d081
Magnus Dybdal <magndy@stud.ntnu.no>
2025-10-20 21:19:33 +0200

    definitions to functions in cpp file

--- framework/Rendering/VertexBuffer.cpp
+++ framework/Rendering/VertexBuffer.cpp
@@ -2,4 +2,30 @@
 
 VertexBuffer::VertexBuffer(const void *vertices, GLsizei size)
 {
+    glGenBuffers(1, &VertexBufferID);
+    glBindBuffer(GL_ARRAY_BUFFER, VertexBufferID);
+    glBufferData(GL_ARRAY_BUFFER,
+                 size,
+                 vertices,
+                 GL_STATIC_DRAW);
+}
+
+VertexBuffer::~VertexBuffer()
+{
+
+}
+
+void VertexBuffer::Bind() const
+{
+    glBindBuffer(GL_ARRAY_BUFFER, *vertices);
+}
+
+void VertexBuffer::Unbind() const
+{
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+void VertexBuffer::BufferSubData(GLintptr offset, GLsizeiptr size, const void *data) const
+{
+
 }
