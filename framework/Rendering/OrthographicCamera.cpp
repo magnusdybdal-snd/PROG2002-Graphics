4ca9d073a7aacc3c659c049b4c888e887902c04c
Magnus Dybdal <magndy@stud.ntnu.no>
2025-10-26 21:21:10 +0100

    implemented projectioncamera cpp

--- framework/Rendering/OrthographicCamera.cpp
+++ framework/Rendering/OrthographicCamera.cpp
@@ -12,5 +12,21 @@ OrthographicCamera::OrthographicCamera(const Frustrum &frustrum, const glm::vec3
 void OrthographicCamera::RecalculateMatrix()
 {
     // Projection matrix
-    this->ProjectionMatrix = glm::ortho()
+    this->ProjectionMatrix = glm::ortho(
+        this->CameraFrustrum.left,
+        this->CameraFrustrum.right,
+        this->CameraFrustrum.bottom,
+        this->CameraFrustrum.top,
+        this->CameraFrustrum.near,
+        this->CameraFrustrum.far
+    );
+
+    // View Matrix
+    glm::mat4 transform = glm::mat4(1.0f);
+    transform = glm::translate(transform, this->Position);
+    transform = glm::rotate(transform, glm::radians(this->Rotation), glm::vec3(0.0f, 0.0f, 1.0f));
+    this->ViewMatrix = glm::inverse(transform);
+
+    // Combined viewprojection matrix
+    this->ViewProjectionMatrix = this->ProjectionMatrix * this->ViewMatrix;
 }
