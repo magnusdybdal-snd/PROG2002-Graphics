a26e751b6b187f5cac54dd1333e25c15d6ca2eac
maeilert <maeilert@stud.ntnu.no>
2025-10-29 15:57:38 +0100

    shader approperiate  gl-version with layout cube fragment shader

--- assignment/shaders/red_cube_fragment.h
+++ assignment/shaders/red_cube_fragment.h
@@ -4,13 +4,12 @@
 #include <string>
 
 #ifdef __APPLE__   // macOS: 4.1 core
-    constexpr const char* GLSL_RED_CUBE_FRAGMENT_VERSION = "#version 410 core\n";
+    constexpr const char* GLSL_RED_CUBE_FRAGMENT_VERSION = "#version 410 core\nuniform samplerCube u_CubeSampler;";
 #else
-    constexpr const char* GLSL_RED_CUBE_FRAGMENT_VERSION = "#version 430 core\n";
+    constexpr const char* GLSL_RED_CUBE_FRAGMENT_VERSION = "#version 430 core\nlayout(binding = 1) uniform samplerCube u_CubeSampler;";
 #endif
 
 inline std::string redCubeFragmentShaderSrc = std::string(GLSL_RED_CUBE_FRAGMENT_VERSION) + R"(
-uniform samplerCube u_CubeSampler;
 
 in vec3 vs_Position;            // INPUT: Position from vertex shader
 uniform vec3 u_Color;
